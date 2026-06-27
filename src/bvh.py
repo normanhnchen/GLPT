b04af06c771648e7832201ffa38de2b81d1c7f16
Norman Chen <209562119+normanhnchen@users.noreply.github.com>
2026-06-27 16:32:33 -0700

    Implement SAH BVH construction

--- src/bvh.py
+++ src/bvh.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 # https://jacco.ompf2.com/2022/04/13/how-to-build-a-bvh-part-1-basics/
+# https://jacco.ompf2.com/2022/04/18/how-to-build-a-bvh-part-2-faster-rays/
 
 
 class BVHNode:
@@ -34,11 +35,32 @@ class BVH:
             node.is_leaf = True
             return
 
-        extent = node.aabb_max - node.aabb_min
-        axis = np.argmax(extent)
+        best_axis = -1
+        best_pos = 0
+        best_cost = np.inf
 
-        indices = self.tri_indices[node.first_tri_idx : node.first_tri_idx + node.tri_count]
-        split_pos = np.mean(self.scene.centroids[indices, axis])
+        # Determine best split position using SAH
+        for axis in range(3):
+            for i in range(node.tri_count):
+                tri_idx = self.tri_indices[i]
+                centroid_pos = self.scene.centroids[tri_idx][axis]
+                cost = self.evaluate_SAH(node, axis, centroid_pos)
+                if cost < best_cost:
+                    best_pos = centroid_pos
+                    best_axis = axis
+                    best_cost = cost
+
+        # Extent of the parent
+        e = node.aabb_max - node.aabb_min
+        parent_area = e[0] * e[1] + e[1] * e[2] + e[2] * e[0]
+        parent_cost = node.tri_count * parent_area
+
+        if (best_cost >= parent_cost):
+            node.is_leaf = True
+            return
+                
+        axis = best_axis
+        split_pos = best_pos
         
         i = node.first_tri_idx
         j = i + node.tri_count - 1
@@ -88,3 +110,42 @@ class BVH:
         tri_verts = self.scene.vertices[self.scene.triangles[indices]]
         node.aabb_min = np.min(tri_verts, axis=(0, 1))
         node.aabb_max = np.max(tri_verts, axis=(0, 1))
+    
+    def evaluate_SAH(self, node, axis, pos):
+        left_box = AABB()
+        right_box = AABB()
+
+        left_count = 0
+        right_count = 0
+
+        for i in range(node.tri_count):
+            tri_idx = self.tri_indices[node.first_tri_idx + i]
+            tri = self.scene.triangles[tri_idx]
+            vertices = self.scene.vertices[tri]
+            centroid = self.scene.centroids[tri_idx]
+            if centroid[axis] < pos:
+                left_box.grow(vertices)
+                left_count += 1
+            else:
+                right_box.grow(vertices)
+                right_count += 1
+        
+        cost = left_count * left_box.get_area() + right_count * right_box.get_area()
+
+        if cost > 0:
+            return cost
+        return np.inf
+
+
+class AABB:
+    def __init__(self):
+        self.min = np.full(3, np.inf)
+        self.max = np.full(3, -np.inf)
+    
+    def grow(self, triangle):
+        self.min = np.minimum(self.min, np.min(triangle, axis=0))
+        self.max = np.maximum(self.max, np.max(triangle, axis=0))
+    
+    def get_area(self):
+        e = self.max - self.min
+        return e[0] * e[1] + e[1] * e[2] + e[2] * e[0]
