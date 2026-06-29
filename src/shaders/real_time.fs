801ad737d0d45f671bad9ebf6e36b7ef7c828273
Norman Chen <209562119+normanhnchen@users.noreply.github.com>
2026-06-28 22:49:38 -0700

    Add texture support

--- src/shaders/real_time.fs
+++ src/shaders/real_time.fs
@@ -1,9 +1,59 @@
 #version 460 core
 
 in vec2 texCoords;
+flat in int matId;
 
 out vec4 fragColor;
 
+struct Material {
+    // Basic material
+    vec3 baseCol;
+    float alpha;
+    vec3 emissive;
+    float metallic;
+    float roughness;
+    // Settings
+    int alphaMode; // 0=OPAQUE, 1=MASK, or 2=BLEND
+    float alphaCutoff;
+    int doubleSided;
+    // Flags
+    int hasEmission;
+    int hasBaseColTex;
+    int hasEmissiveTex;
+    int hasRoughTex;
+    int hasMetalTex;
+    int hasNormalTex;
+    int hasOcclTex;
+    // Texture IDs
+    int baseTexId;
+    int emissiveTexId;
+    int roughTexId;
+    int metalTexId;
+    int normalTexId;
+    int occlTexId;
+    // glTF extensions
+    float emissiveStrength;
+    float transmission;
+    float ior;
+};
+
+layout (std430, binding = 0) buffer MaterialBuffer {
+    Material materials[];
+};
+
+layout(binding = 0) uniform sampler2DArray baseColorTextures;
+layout(binding = 1) uniform sampler2DArray emissiveTextures;
+layout(binding = 2) uniform sampler2DArray roughnessTextures;
+layout(binding = 3) uniform sampler2DArray metallicTextures;
+layout(binding = 4) uniform sampler2DArray normalTextures;
+layout(binding = 5) uniform sampler2DArray occlusionTextures;
+
 void main() {
-    fragColor = vec4(texCoords, 1.0, 1.0);
+    Material mat = materials[matId];
+    if (mat.hasBaseColTex == 1) {
+        vec4 baseCol = texture(baseColorTextures, vec3(texCoords, mat.baseTexId));
+        mat.baseCol = baseCol.rgb;
+        mat.alpha = baseCol.w;
+    }
+    fragColor.rgb = mat.baseCol;
 }
