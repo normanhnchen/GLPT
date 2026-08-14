ae09084fa41f9adc47f6de7647c7f6e520c70e37
Norman Chen <209562119+normanhnchen@users.noreply.github.com>
2026-08-13 20:26:54 -0700

    Initialize a settings dialog window

--- src/launcher.py
+++ src/launcher.py
@@ -1,7 +1,8 @@
 import sys
 from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QWidget, QVBoxLayout, 
-    QHBoxLayout, QLabel, QPushButton, QSlider, QCheckBox, QStackedWidget
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QGroupBox,
+    QHBoxLayout, QLabel, QPushButton, QSlider, QCheckBox, QStackedWidget,
+    QDialog,
 )
 from PySide6.QtCore import Qt
 
@@ -14,16 +15,33 @@ QWidget {
 }
 
 QLabel#titleLabel {
-    font-size: 48px;
+    font-size: 96px;
 }
 
 QPushButton {
     background-color: #4c79a6;
-    font-size: 24px;
+    font-size: 36px;
     border-radius: 8px;
-    padding: 8px 0px;
+    padding: 16px 4px;
+}
+
+QPushButton:hover {
+    background-color: #7ca4cc;
 }
 """
+
+
+class SettingsDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Settings")
+        self.resize(600, 400)
+
+        self.init_settings()
+
+    def init_settings(self):
+        pass
+
 
 class Launcher(QMainWindow):
     def __init__(self):
@@ -34,14 +52,16 @@ class Launcher(QMainWindow):
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
-        self._init_menu_screen()
+        self.init_menu()
 
         self.stacked_widget.addWidget(self.menu_widget)
     
-    def _init_menu_screen(self):
+    def init_menu(self):
         self.menu_widget = QWidget()
         layout = QVBoxLayout(self.menu_widget)
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.settings_dialog = None
 
         title = QLabel("GLPT")
         title.setObjectName("titleLabel")
@@ -50,12 +70,31 @@ class Launcher(QMainWindow):
         layout.addSpacing(50)
 
         button_run = QPushButton("Run")
-        button_run.setFixedWidth(200)
-        button_run.clicked.connect(self._on_button_run)
+        button_run.setFixedWidth(300)
+        button_run.clicked.connect(self.run)
         layout.addWidget(button_run, alignment=Qt.AlignmentFlag.AlignCenter)
 
-    def _on_button_run(self):
+        button_settings = QPushButton("Settings")
+        button_settings.setFixedWidth(300)
+        button_settings.clicked.connect(self.open_settings)
+        layout.addWidget(button_settings, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    def run(self):
         print(0)
+
+    def open_settings(self):
+        """
+        Open a settings dialog window in a way which doesn't block the main application.
+        """
+
+        if not self.settings_dialog:
+            self.settings_dialog = SettingsDialog()
+
+        self.settings_dialog.show()
+        # Bring settings dialog to the front
+        self.settings_dialog.raise_()
+        self.settings_dialog.activateWindow()
+
 
 
 def main():
