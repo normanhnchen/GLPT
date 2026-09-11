Add variance buffers for the AI denoiser TODO: Fix remaining bugs from this implementation

src/ai/denoiser/network.py
@@ -101,9 +101,12 @@ class UNet(nn.Module):
 
 # See 9.3 KPCN
 class KPCN(nn.Module):
-    def __init__(self, in_channels=13, kernel_size=21):
+    def __init__(self, in_channels=26, kernel_size=21):
         """
-        The default in_channels is 13: diffuse(3) + specular(3) + albedo(3) + normal(3) + depth(1).
+        The default in_channels is 26:
+        diffuse(3) + specular(3) + albedo(3) + normal(3) + depth(1)
+        + diffuse_var(3) + specular_var(3) + albedo_var(3) + normal_var(3) + depth_var(1),
+        where a buffer with var is the per-pixel variance.
         kernel_size=21 per Bako et al. U-Net output is kernel_size**2 channels: one predicted
         set of weights based on the neighborhood per pixel.
         """
@@ -184,15 +187,53 @@ class KPCN(nn.Module):
         # Apply weights to the combined image RGB channels
         return (output * w).sum(dim=2) # (B, 3, H, W)
 
-    def denoise(self, diffuse, specular, albedo, normal, depth, denoised):
+    def denoise(
+        self,
+        diffuse,
+        specular,
+        albedo,
+        normal,
+        depth,
+        diffuse_sq,
+        specular_sq,
+        albedo_sq,
+        normal_sq,
+        depth_sq,
+        denoised
+    ):
         with torch.no_grad():
             self.eval()
             # Convert from OpenGL textures to torch tensors
-            diffuse = self._tex_to_tensor(diffuse, keep_channels=3) # RGBA -> RGB
+            diffuse_full = self._tex_to_tensor(diffuse, keep_channels=4) # RGBA -> RGBA
+            diffuse = diffuse_full[..., :3] # RGBA -> RGB
             specular = self._tex_to_tensor(specular, keep_channels=3) # RGBA -> RGB
             albedo = self._tex_to_tensor(albedo, keep_channels=3) # RGBA -> RGB
             normal = self._tex_to_tensor(normal, keep_channels=3) # RGBA -> RGB
             depth = self._tex_to_tensor(depth, keep_channels=1) # RGBA -> R
+
+            diffuse_sq = self._tex_to_tensor(diffuse_sq, keep_channels=3) # RGBA -> RGB
+            specular_sq = self._tex_to_tensor(specular_sq, keep_channels=3) # RGBA -> RGB
+            albedo_sq = self._tex_to_tensor(albedo_sq, keep_channels=3) # RGBA -> RGB
+            normal_sq = self._tex_to_tensor(normal_sq, keep_channels=3) # RGBA -> RGB
+            depth_sq_full = self._tex_to_tensor(depth_sq, keep_channels=2) # RGBA -> RG
+
+            # We saved the number of depth samples here on the shader side
+            depth_samples = depth_sq_full[:, 1] # RG -> G
+            # We saved the number of total samples here on the shader side
+            total_samples = diffuse_full[..., 3] # RGBA -> A
+
+            depth_sq = depth_sq_full[:, 0]
+
+            diffuse_variance = self.calculate_variance(diffuse, diffuse_sq, total_samples)
+            specular_variance = self.calculate_variance(specular, specular_sq, total_samples)
+            albedo_variance = self.calculate_variance(albedo, albedo_sq, total_samples)
+            normal_variance = self.calculate_variance(normal, normal_sq, total_samples)
+            depth_variance = self.calculate_variance(depth, depth_sq, depth_samples)
+
+            # Taylor-approximation transformation per Bako et al.
+            diffuse_variance /= (albedo + settings.ai_training.epsilon) ** 2
+            # Add small offset to prevent division by zero
+            specular_variance /= specular ** 2 + settings.ai_training.epsilon
 
             # Normalize depth via the inverse depth method
             depth = self.normalize_depth(depth)
@@ -203,8 +244,19 @@ class KPCN(nn.Module):
             diffuse_compressed = self.compress(diffuse_demodulated)
             specular_compressed = self.compress(specular_linear)
 
-            # 13 channels
-            x = torch.cat([diffuse_compressed, specular_compressed, albedo, normal, depth], dim=1)
+            # 26 channels
+            x = torch.cat([
+                diffuse_compressed,
+                specular_compressed,
+                albedo,
+                normal,
+                depth,
+                diffuse_variance,
+                specular_variance,
+                albedo_variance,
+                normal_variance,
+                depth_variance
+            ], dim=1)
 
             diffuse_predicted, specular_predicted = self(x, diffuse_demodulated, specular_compressed)
 
@@ -262,7 +314,11 @@ class KPCN(nn.Module):
         return inv
 
     def demodulate(self, x, albedo):
-        return x / albedo.clamp(min=0.00316)
+        return x / albedo.clamp(min=settings.ai_training.epsilon)
 
     def remodulate(self, x, albedo):
-        return x * albedo.clamp(min=0.00316)
+        return x * albedo.clamp(min=settings.ai_training.epsilon)
+
+    def calculate_variance(self, mean, mean_sq, n):
+        # Clamp values to prevent negative variance or division by zero
+        return (mean_sq - mean ** 2).clamp(min=0) * (n / min(n - 1, 1))

src/ai/training/training.py
@@ -107,6 +107,12 @@ class DenoiseDataset(Dataset):
         self.target_diffuse_path = renders_path / "target_diffuse/"
         self.target_specular_path = renders_path / "target_specular/"
 
+        self.diffuse_sq_path = renders_path / "diffuse_sq/"
+        self.specular_sq_path = renders_path / "specular_sq/"
+        self.albedo_sq_path = renders_path / "albedo_sq/"
+        self.normal_sq_path = renders_path / "normal_sq/"
+        self.depth_sq_path = renders_path / "depth_sq/"
+
         self.num_samples = sum(1 for item in self.diffuse_path.iterdir() if item.is_file())
         self.patch_size = patch_size
         self.patches_per_image = patches_per_image
@@ -128,7 +134,8 @@ class DenoiseDataset(Dataset):
 
         # Convert EXR images to PyTorch tensors
         # -------------------------------------
-        diffuse = exr_to_tensor(diffuse, keep_channels=3)
+        diffuse_full = exr_to_tensor(diffuse, keep_channels=4)
+        diffuse = diffuse_full[..., :3]
         specular = exr_to_tensor(specular, keep_channels=3)
         albedo = exr_to_tensor(albedo, keep_channels=3)
         normal = exr_to_tensor(normal, keep_channels=3)
@@ -136,10 +143,39 @@ class DenoiseDataset(Dataset):
         target_diffuse = exr_to_tensor(target_diffuse, keep_channels=3)
         target_specular = exr_to_tensor(target_specular, keep_channels=3)
 
+        diffuse_sq = exr_to_tensor(load_exr(self.diffuse_sq_path / f"diffuse_sq_{idx}.exr"), keep_channels=3)
+        specular_sq = exr_to_tensor(load_exr(self.specular_sq_path / f"specular_sq_{idx}.exr"), keep_channels=3)
+        albedo_sq = exr_to_tensor(load_exr(self.albedo_sq_path / f"albedo_sq_{idx}.exr"), keep_channels=3)
+        normal_sq = exr_to_tensor(load_exr(self.normal_sq_path / f"normal_sq_{idx}.exr"), keep_channels=3)
+        depth_sq_full = exr_to_tensor(load_exr(self.depth_sq_path / f"depth_sq_{idx}.exr"), keep_channels=2)
+        depth_sq = depth_sq_full[0]
+
+        # We saved the number of depth samples here on the shader side
+        depth_samples = depth_sq_full[:, 1]
+        # We saved the number of total samples here on the shader side
+        total_samples = diffuse_full[..., 3]
+
+        diffuse_variance = denoiser.calculate_variance(diffuse, diffuse_sq, total_samples)
+        specular_variance = denoiser.calculate_variance(specular, specular_sq, total_samples)
+        albedo_variance = denoiser.calculate_variance(albedo, albedo_sq, total_samples)
+        normal_variance = denoiser.calculate_variance(normal, normal_sq, total_samples)
+        depth_variance = denoiser.calculate_variance(depth, depth_sq, depth_samples)
+
         # Normalize depth via the inverse depth method
         depth = denoiser.normalize_depth(depth)
 
-        x = torch.cat([diffuse, specular, albedo, normal, depth])
+        x = torch.cat([
+            diffuse,
+            specular,
+            albedo,
+            normal,
+            depth,
+            diffuse_variance,
+            specular_variance,
+            albedo_variance, 
+            normal_variance,
+            depth_variance
+        ])
         target = torch.cat([target_diffuse, target_specular])
 
         if self.is_validation:
@@ -209,6 +245,16 @@ def _preprocess(x, target):
     albedo = x[:, 6:9]
     normal = x[:, 9:12]
     depth = x[:, 12:13]
+    diffuse_variance = x[:, 13:16]
+    specular_variance = x[:, 16:19]
+    albedo_variance = x[:, 19:22]
+    normal_variance = x[:, 22:25]
+    depth_variance = x[:, 25:26]
+
+    # Taylor-approximation transformation per Bako et al.
+    diffuse_variance /= (albedo + settings.ai_training.epsilon) ** 2
+    # Add small offset to prevent division by zero
+    specular_variance /= specular ** 2 + settings.ai_training.epsilon
 
     target_diffuse  = target[:, :3]
     target_specular = target[:, 3:6]
@@ -219,7 +265,18 @@ def _preprocess(x, target):
     diffuse_compressed = denoiser.compress(diffuse_demodulated)
     specular_compressed = denoiser.compress(specular_linear)
 
-    x = torch.cat([diffuse_compressed, specular_compressed, albedo, normal, depth], dim=1)
+    x = torch.cat([
+        diffuse_compressed,
+        specular_compressed,
+        albedo,
+        normal,
+        depth,
+        diffuse_variance,
+        specular_variance,
+        albedo_variance,
+        normal_variance,
+        depth_variance
+    ], dim=1)
 
     target_linear = target_diffuse + target_specular
 

src/states/render.py
@@ -18,6 +18,12 @@ class FramebufferState:
         self.saved_normal = None
         self.saved_depth = None
 
+        self.saved_diffuse_sq = None
+        self.saved_specular_sq = None
+        self.saved_albedo_sq = None
+        self.saved_normal_sq = None
+        self.saved_depth_sq = None
+
     def _create_active_buffers(self):
         self.combined = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
         self.diffuse = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
@@ -25,6 +31,12 @@ class FramebufferState:
         self.albedo = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
         self.normal = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
         self.depth = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+
+        self.diffuse_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.specular_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.albedo_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.normal_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.depth_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
     
     def _create_saved_buffers(self):
         self.saved_combined = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
@@ -34,6 +46,12 @@ class FramebufferState:
         self.saved_normal = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
         self.saved_depth = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
 
+        self.saved_diffuse_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.saved_specular_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.saved_albedo_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.saved_normal_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+        self.saved_depth_sq = self.ctx.texture(settings.screen.resolution, 4, dtype=f4)
+
     def _clear_active_buffers(self):
         zeros = np.zeros((*settings.screen.resolution, 4), dtype=f4)
         self.combined.write(zeros)
@@ -42,6 +60,12 @@ class FramebufferState:
         self.albedo.write(zeros)
         self.normal.write(zeros)
         self.depth.write(zeros)
+
+        self.diffuse_sq.write(zeros)
+        self.specular_sq.write(zeros)
+        self.albedo_sq.write(zeros)
+        self.normal_sq.write(zeros)
+        self.depth_sq.write(zeros)
 
     def _release_saved_buffers(self):
         if self.saved_combined is not None:
@@ -57,6 +81,17 @@ class FramebufferState:
         if self.saved_depth is not None:
             self.saved_depth.release()
 
+        if self.saved_diffuse_sq is not None:
+            self.saved_diffuse_sq.release()
+        if self.saved_specular_sq is not None:
+            self.saved_specular_sq.release()
+        if self.saved_albedo_sq is not None:
+            self.saved_albedo_sq.release()
+        if self.saved_normal_sq is not None:
+            self.saved_normal_sq.release()
+        if self.saved_depth_sq is not None:
+            self.saved_depth_sq.release()
+
     def _release_active_buffers(self):
         if self.combined is not None:
             self.combined.release()
@@ -70,6 +105,17 @@ class FramebufferState:
             self.normal.release()
         if self.depth is not None:
             self.depth.release()
+
+        if self.diffuse_sq is not None:
+            self.diffuse_sq.release()
+        if self.specular_sq is not None:
+            self.specular_sq.release()
+        if self.albedo_sq is not None:
+            self.albedo_sq.release()
+        if self.normal_sq is not None:
+            self.normal_sq.release()
+        if self.depth_sq is not None:
+            self.depth_sq.release()
     
     def reset(self):
         self._release_active_buffers()
@@ -87,6 +133,12 @@ class FramebufferState:
         self.saved_normal.write(self.normal.read())
         self.saved_depth.write(self.depth.read())
 
+        self.saved_diffuse_sq.write(self.diffuse_sq.read())
+        self.saved_specular_sq.write(self.specular_sq.read())
+        self.saved_albedo_sq.write(self.albedo_sq.read())
+        self.saved_normal_sq.write(self.normal_sq.read())
+        self.saved_depth_sq.write(self.depth_sq.read())
+
     def bind_to_images(
             self,
             combined_loc=0,
@@ -94,7 +146,12 @@ class FramebufferState:
             specular_loc=2,
             albedo_loc=3,
             normal_loc=4,
-            depth_loc=5
+            depth_loc=5,
+            diffuse_sq_loc=6,
+            specular_sq_loc=7,
+            albedo_sq_loc=8,
+            normal_sq_loc=9,
+            depth_sq_loc=10
         ):
         self.combined.bind_to_image(combined_loc, read=True, write=True)
         self.diffuse.bind_to_image(diffuse_loc, read=True, write=True)
@@ -102,6 +159,11 @@ class FramebufferState:
         self.albedo.bind_to_image(albedo_loc, read=True, write=True)
         self.normal.bind_to_image(normal_loc, read=True, write=True)
         self.depth.bind_to_image(depth_loc, read=True, write=True)
+        self.diffuse_sq.bind_to_image(diffuse_sq_loc, read=True, write=True)
+        self.specular_sq.bind_to_image(specular_sq_loc, read=True, write=True)
+        self.albedo_sq.bind_to_image(albedo_sq_loc, read=True, write=True)
+        self.normal_sq.bind_to_image(normal_sq_loc, read=True, write=True)
+        self.depth_sq.bind_to_image(depth_sq_loc, read=True, write=True)
 
     def _get_ndarray(self, buffer):
         data = buffer.read()
@@ -132,6 +194,21 @@ class FramebufferState:
 
     def get_ndarray_depth(self):
         return self._get_ndarray(self.depth)
+
+    def get_ndarray_diffuse_sq(self):
+        return self._get_ndarray(self.diffuse_sq)
+
+    def get_ndarray_specular_sq(self):
+        return self._get_ndarray(self.specular_sq)
+
+    def get_ndarray_albedo_sq(self):
+        return self._get_ndarray(self.albedo_sq)
+
+    def get_ndarray_normal_sq(self):
+        return self._get_ndarray(self.normal_sq)
+
+    def get_ndarray_depth_sq(self):
+        return self._get_ndarray(self.depth_sq)
 
 
 class TileState:
@@ -221,10 +298,35 @@ class DenoiseState:
         if self.saved_denoised is not None:
             self.saved_denoised.release()
 
-    def denoise(self, ai_denoiser, diffuse, specular, albedo, normal, depth):
+    def denoise(
+            self,
+            ai_denoiser,
+            diffuse,
+            specular,
+            albedo,
+            normal,
+            depth,
+            diffuse_sq,
+            specular_sq,
+            albedo_sq,
+            normal_sq,
+            depth_sq
+        ):
         if self.saved_denoised is None:
             self.saved_denoised = self.ctx.texture(settings.screen.resolution, 3, dtype=f4)
-            ai_denoiser.denoise(diffuse, specular, albedo, normal, depth, self.saved_denoised)
+            ai_denoiser.denoise(
+                diffuse,
+                specular,
+                albedo,
+                normal,
+                depth,
+                diffuse_sq,
+                specular_sq,
+                albedo_sq,
+                normal_sq,
+                depth_sq,
+                self.saved_denoised
+            )
 
     def reset(self):
         self._release_buffer()
@@ -286,7 +388,12 @@ class PTState:
             self.framebuffers.saved_specular,
             self.framebuffers.saved_albedo,
             self.framebuffers.saved_normal,
-            self.framebuffers.saved_depth
+            self.framebuffers.saved_depth,
+            self.framebuffers.saved_diffuse_sq,
+            self.framebuffers.saved_specular_sq,
+            self.framebuffers.saved_albedo_sq,
+            self.framebuffers.saved_normal_sq,
+            self.framebuffers.saved_depth_sq
         )
 
 